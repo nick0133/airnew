e65dc62c99f34bcedc769acc37f92e7854d572f9
Yusup-beep <yusupyedyyew@gmail.com>
2024-11-07 00:32:35 +0500

    table fix

--- app/Filament/Resources/BlockResource.php
+++ app/Filament/Resources/BlockResource.php
@@ -14,6 +14,7 @@ use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -45,7 +46,8 @@ class BlockResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('name')->searchable(),
+                TextColumn::make('category.name'),
             ])
             ->filters([
                 //
